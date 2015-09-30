75e82a6cc4aaa35666fd0fd9ac814c32cbe38d4a
dymondcori <cdymond@live.unc.edu>
2015-09-30 16:26:58 -0400

    updating

---
+++ js/script2.js
@@ -0,0 +1,21 @@
+function setupSlider (){
+//takes html and using slider jquery to make it a slider
+
+  addQuiz;
+}
+
+function addQuiz () {
+ //handles the newquiz code
+
+  addScroll
+}
+
+function addScroll () {
+ //adds a scroll bar to the newquiz code
+
+  alert ("done");
+}
+
+$(Document).ready(){
+  setupSlider ();  //kickoff
+}
---
+++ js/script3.js
@@ -0,0 +1,30 @@
+function setupSlider (){
+//takes html and using slider jquery to make it a slider
+    $( selector ).cbpFWSlider( [options] );
+    - options:
+    {
+      // default transition speed (ms)
+      speed : 500,
+      // default transition easing
+      easing : 'ease'
+    }
+    - destroy:
+    $( selector ).cbpFWSlider( 'destroy' );
+  addQuiz;
+}
+
+function addQuiz () {
+ //handles the newquiz code
+
+  addScroll
+}
+
+function addScroll () {
+ //adds a scroll bar to the newquiz code
+
+  alert ("done");
+}
+
+$(Document).ready(){
+  setupSlider ();  //kickoff
+}
